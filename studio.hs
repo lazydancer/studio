715acd279f4a0be8e5c0d5b2d8879a0767bac2e1
Lazydancer <jrpucula@gmail.com>
2013-06-11 20:53:27 -0400

    Starting to wrap the functions together

--- studio.hs
+++ studio.hs
@@ -4,17 +4,23 @@ import Text.Pandoc
 import Control.Monad(forM)
 import System.Directory (doesDirectoryExist, getDirectoryContents)
 import System.FilePath ((</>), takeExtension)
+import System.IO(writeFile)
+-- Access to the command line commands, Want to try and do it the haskell way, 
+-- a lot easier this way for now
+import System.Cmd
+import System.Exit
 
--- Access to the command line commands, Want to try and do it the haskell way
---import System.Cmd
---import System.Exit
+--Copies a directory to a destination from system command, somewhat dangerous (constants added)
+--"Out" folder must exist
+copyDir :: IO ExitCode
+copyDir = system $ "cp -r ~/Dropbox/Projects/Site/Studio/In/* ~/Dropbox/Projects/Site/Studio/Out/"
 
-getMarkdownPreset :: IO [FilePath]
 --;;;;;;Preset Markdown
-getMarkdownPreset = getMarkdown "/Users/james/Dropbox/Projects/Site/Studio/In/"
+getMarkdownPreset :: IO [FilePath]
+getMarkdownPreset = getMarkdown "/Users/james/Dropbox/Projects/Site/Studio/Out/"
 
-getMarkdown :: FilePath -> IO [FilePath]
 --Finds all the files in a directory and filters out the markdown files
+getMarkdown :: FilePath -> IO [FilePath]
 getMarkdown topdir = do
   names <- getDirectoryContents topdir
   let properNames = filter (`notElem` [".","..",".DS_Store"]) names
@@ -27,21 +33,21 @@ getMarkdown topdir = do
   let mdPaths = filter ((".markdown" ==)  . takeExtension)  (concat paths)
   return mdPaths
 
-convertMdtoHtml :: IO ()
---Reads file for now using Bitcoin as a test to an html string
-convertMdtoHtml = do
-  contents <- readFile "/Users/james/Dropbox/Projects/Site/Studio/In/2012/Bitcoin/index.markdown"
+--Takes a markdown file and writes a html file, same name in same dir
+convertMdtoHtml :: FilePath -> IO () 
+convertMdtoHtml file = do
+  contents <- readFile file 
   let pandoc = readMarkdown def contents
-  let html = writeHtmlString siteOptions pandoc
-  putStrLn html
+  template <- readFile "/Users/james/Dropbox/Projects/Site/Studio/In/template.html" 
+  let html = writeHtmlString (siteOptions template) pandoc
+  writeFile (file ++ ".html") html
 
-siteOptions :: WriterOptions
+convertArticles :: IO ()
+convertArticles = undefined
+
 --Site Options with all default except following
-siteOptions = def { writerStandalone = True, writerTemplate = template }
-
-template :: String
-template = "<!DOCTYPE html>\n<head>\n <meta charset=\"utf-8\">\n  <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge,chrome=1\">\n  <title>$title$</title>\n  <meta name=\"description\" content=\"\">\n  <meta name=\"viewport\" content=\"width=device-width\">\n  <link rel=\"stylesheet\" href=\"normalize.css\">\n  <link rel=\"stylesheet\" href=\"main.css\" />\n</head>\n<body>\n  <header>\n    <a class=\"logo\" href=\"/\"></a> \n  </header>\n  <article>\n    $body$ \n  </article>\n</body>\n</html>" 
-
+siteOptions :: String -> WriterOptions
+siteOptions template = def { writerStandalone = True, writerTemplate = template }
 
 {-
   Copy Folder to new location
