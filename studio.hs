784d53c26ddd5daba080493a4d61a1e4f9f94719
Lazydancer <jrpucula@gmail.com>
2013-07-14 12:56:39 -0400

    The main program is now mostly in the main

--- studio.hs
+++ studio.hs
@@ -1,10 +1,10 @@
 import Text.Pandoc
-
-import Control.Monad (forM_, join)
+import Control.Monad (forM_, join, mapM)
 import System.Directory (getDirectoryContents, createDirectoryIfMissing, setCurrentDirectory, copyFile)
 import System.FilePath (takeExtension, takeFileName, dropExtension)
 import System.IO (writeFile)
-
+import Data.List (intercalate)
+import Data.Char (toLower)
 import Control.Applicative ((<$>))
 import GHC.Exts (sortWith)
 
@@ -12,50 +12,48 @@ main :: IO ()
 main = do
   setCurrentDirectory "/Users/james/Dropbox/Projects/Site/Studio"
   createDirectoryIfMissing False "Output"
-  mdFiles <- getArticles
-  mapM_ writeArticle mdFiles
-  writeTOC mdFiles
-  moveStatic
-
-getArticles :: IO [FilePath]
-getArticles = filter (`notElem` [".","..",".DS_Store"]) <$> getDirectoryContents "Articles"
-
-writeArticle :: FilePath -> IO ()
-writeArticle file = do 
-  pandoc <- readMarkdown def <$> readFile ("Articles/" ++ file) 
-  let year = inlineStr $ [last $ docDate $ meta pandoc]  
   template <- readFile "template.html"
-  let html = writeHtmlString (siteOptions template) pandoc
-  createDirectoryIfMissing True $ "Output/" ++ year ++ "/" ++ dropExtension (takeFileName file)
-  writeFile ("Output/" ++ year ++ "/" ++ dropExtension (takeFileName file) ++ "/index.html") html
+  
+  articles <- filter (`notElem` [".","..",".DS_Store"]) <$> getDirectoryContents "Articles"
 
-writeTOC :: [FilePath] -> IO ()
-writeTOC mdFiles = do
-  list <- mapM getItem mdFiles
-  let olist = orderList $ listOrd list
-  template <- readFile "template.html"
-  let tocPan = Pandoc Meta{docTitle = [], docAuthors = [], docDate = []} ([Plain [RawInline "html" "<div class=\"toc\">"]] ++ [BulletList olist] ++ [Plain [RawInline "html" "</div>"]]) 
+  unparsedArt <- mapM readFile $ map ("Articles/" ++) articles 
+  let pandocArt = map (readMarkdown def) unparsedArt
+  list <- mapM (getItem template) pandocArt 
+  let tocPan = Pandoc Meta{docTitle = [], docAuthors = [], docDate = []} ([Plain [RawInline "html" "<div class=\"toc\">"]] ++ [BulletList (orderList list)] ++ [Plain [RawInline "html" "</div>"]]) 
   let html = writeHtmlString (siteOptions template) tocPan 
   writeFile "Output/index.html" html
-
-moveStatic :: IO ()
-moveStatic = do
+  
   cpf <-  filter ((`elem` [".css",".js",".png",".jpg"]) . takeExtension) <$> getDirectoryContents "."
   forM_ cpf (\x -> copyFile x ("Output/" ++ x))
 
---Orders the toc list by the date, reverse chronological
-listOrd :: [([Inline],[Block])] -> [(Integer,[Block])]
-listOrd = map ea
-  where ea (fs,ls) = (dateOrd $ inlineStrb fs, ls)
+  
+--Write the article and return information for TOC
+getItem :: String -> Pandoc -> IO ([Inline],[Block])
+getItem template pandoc = do
+  let title = docTitle $ meta pandoc
+  let date = docDate $ meta pandoc  
+  let year = inlineStr $ [last date]
+
+  let html = writeHtmlString (siteOptions template) pandoc
+  createDirectoryIfMissing True $ "Output/" ++ year ++ "/" ++ urlName title 
+  writeFile ("Output/" ++ year ++ "/" ++ urlName title ++ "/index.html") html
+
+  return (date,[Plain 
+            ([RawInline "html" "<span>"] ++ 
+              date ++
+                [RawInline "html" "</span>"] ++ 
+                  [Link title 
+                    ("/" ++ year ++ "/" ++ urlName title,"")])])
 
 --Orders the tuples and returns the a ordered list of TOC elements
-orderList :: [(Integer,[Block])] ->  [[Block]]
-orderList xs = reverse $ snd $ unzip $ sortWith fst xs 
+orderList :: [([Inline],[Block])] ->  [[Block]]
+orderList = reverse . snd . unzip . sortWith fst . map ea 
+  where ea (fs,ls) = (dateOrd $ inlineStrb fs, ls)
 
 --Convert docDate into a number to be able to order
 --Format: Jan 02 2013
 dateOrd :: [String] -> Integer
-dateOrd [month,day,year] = read $ join $ map monthCnvt[year,month,day] 
+dateOrd [month,day,year] = read $ join [year,monthCnvt month,day] 
 
 --Convert the month to its repective date number
 monthCnvt :: String -> String 
@@ -72,30 +70,17 @@ monthCnvt x
   | "Oct" == x = "10"
   | "Nov" == x = "11"
   | "Dec" == x = "12"
-  | otherwise  = x
-
 
 inlineStr :: [Inline] -> String
 inlineStr = foldl fn ""
   where fn ys (Str x) = ys ++ x 
         fn ys (Space ) = ys ++ " "
 
+inlineStrb :: [Inline] -> [String]
 inlineStrb = words . inlineStr 
   
---The block is returned with an inline date for ordering
-getItem :: FilePath -> IO ([Inline],[Block])
-getItem file = do
-  pandoc <- readMarkdown def <$> readFile ("Articles/" ++ file)
-  let title = docTitle $ meta pandoc
-  let date = docDate $ meta pandoc  
-  let year = inlineStr $ [last date]
-  return (date,[Plain 
-            ([RawInline "html" "<span>"] ++ 
-              date ++
-                [RawInline "html" "</span>"] ++ 
-                  [Link title 
-                    ("/" ++ year ++ "/" ++ dropExtension (takeFileName file),"")])])
-
+urlName :: [Inline] -> String
+urlName = map toLower . intercalate "-" . inlineStrb 
 
 meta :: Pandoc -> Meta
 meta (Pandoc x _) = x
