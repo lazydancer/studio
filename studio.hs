5b1668d7c349a010ddd1e0021d73a77807ca90c7
Lazydancer <jrpucula@gmail.com>
2013-10-12 18:49:31 -0400

    Archive page created, article on front page

--- studio.hs
+++ studio.hs
@@ -18,32 +18,26 @@ data Page = Page { title :: FilePath    -- Path to words
                  , date    :: [String]    -- Month,day,year
                  } deriving (Show) 
 
-
 main :: IO () 
 main = do
-  --Init
-  --setCurrentDirectory "/Users/james/Dropbox/Projects/Site/Studio"
   createDirectoryIfMissing False "Output"
   template <- readFile "template.html"
   
-  --Get Articles
   articles <- getDirectoryContents "Articles"
   let articles' = filter (`notElem` [".","..",".DS_Store"]) articles 
 
   pages <- getPages articles'
-  
-  --Build Articles
-  mapM_ (writePage template) pages
+  let pages' = orderPages pages
 
-  --Move over the images
-    --articles
+  mapM_ (writePage template) pages'
+  writeRecentPage template (head pages') 
+
   mapM_ moveStatic pages
-    --base dir
+
   files <-  filter ((`elem` [".css",".js"]) . takeExtension) <$> getDirectoryContents "."
   forM_ files (\x -> copyFile x ("Output/" ++ x))
   
-  --Table of contents, main page
-  writeTOC template pages
+  writeTOC template pages'
 
 getPages :: [FilePath] -> IO [Page]
 getPages = mapM (\x -> do static <- getStatic ("Articles/" ++ x)
@@ -51,7 +45,7 @@ getPages = mapM (\x -> do static <- getStatic ("Articles/" ++ x)
                           return (Page x static date))
 
 writePage :: String -> Page -> IO ()
-writePage template page= do
+writePage template page = do
   let year = last $ date page  
 
   mdArt <- readFile ("Articles/" ++ title page ++ "/words.md")
@@ -59,6 +53,28 @@ writePage template page= do
   let html = writeHtmlString (siteOptions template) pandocArt
   createDirectoryIfMissing True $ "Output/" ++ year ++ "/" ++ title page 
   writeFile ("Output/" ++ year ++ "/" ++ title page ++ "/index.html") html
+
+--order the pages by date
+orderPages :: [Page] -> [Page]
+orderPages = reverse . sortWith (\x -> dateNumber (date x))
+  where 
+    dateNumber [month,day,year] = read $ join [year,monthCnvt month,day] :: Integer
+
+--Convert the month to its repective date number in string
+monthCnvt :: String -> String 
+monthCnvt x | length monthNum < 10 = "0" ++ monthNum
+            | otherwise            = monthNum
+  where monthNum = show $ fromEnum (read x :: Month)
+
+writeRecentPage :: String -> Page -> IO ()
+writeRecentPage template page = do
+  let year = last $ date page  
+
+  mdArt <- readFile ("Articles/" ++ title page ++ "/words.md")
+  let pandocArt = readMarkdown def mdArt
+  let html = writeHtmlString (siteOptions template) pandocArt
+  writeFile ("Output/index.html") html
+
 
 getPageDate :: FilePath -> IO [String] 
 getPageDate article = do
@@ -79,13 +95,12 @@ moveStatic page = mapM_ func (static page)
 writeTOC :: String -> [Page] -> IO ()
 writeTOC template pages = do
   list <- mapM getItem pages 
-  let list' = orderList list
-  let html = writeHtmlString (siteOptions template) (tocWrap list') 
-  createDirectoryIfMissing True $ "Output/archives/" 
+  let html = writeHtmlString (siteOptions template) (tocWrap list) 
+  createDirectoryIfMissing True $ "Output/archives" 
   writeFile "Output/archives/index.html" html 
 
 --Write the article and return information for TOC
-getItem :: Page -> IO ([Inline],[Block])
+getItem :: Page -> IO [Block]
 getItem page = do
   mdArt <- readFile ("Articles/" ++ title page ++ "/words.md")
   let pandoc = readMarkdown def mdArt
@@ -93,31 +108,15 @@ getItem page = do
   let date = docDate $ meta pandoc  
   let year = unwords $ inlineStr [last date]
 
-  return (date,[Plain 
-            ([RawInline "html" "<span>"] ++ 
-              date ++
-                [RawInline "html" "</span>"] ++ 
-                  [Link (docTitle $ meta pandoc)
-                    ("/" ++ year ++ "/" ++ title page,"")])])
+  return [Plain 
+           ([RawInline "html" "<span>"] ++ 
+             date ++
+               [RawInline "html" "</span>"] ++ 
+                 [Link (docTitle $ meta pandoc)
+                   ("/" ++ year ++ "/" ++ title page,"")])]
 
 
---Converting and then sorting with the first element
---This is done by converting the date into a number
---The second element is returned
-orderList :: [([Inline],[Block])] -> [[Block]]
-orderList = reverse . snd . unzip . sortWith fst . map ea 
-  where ea (fs,ls) = (dateOrd $ inlineStr fs, ls)
 
---Convert docDate into a number to be able to order
---Format: Jan 02 2013 = 20130102
-dateOrd :: [String] -> Integer
-dateOrd [month,day,year] = read $ join [year,monthCnvt month,day] 
-
---Convert the month to its repective date number in string
-monthCnvt :: String -> String 
-monthCnvt x | length monthNum < 10 = "0" ++ monthNum
-            | otherwise            = monthNum
-  where monthNum = show $ fromEnum (read x :: Month)
 
 --Convert Pandocs Inline to a string with " " for Space, or into words, or
 -- into a urlName format
