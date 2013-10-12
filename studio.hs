9e148cfd0214d4bee3015304567783436cd3ac3a
Lazydancer <jrpucula@gmail.com>
2013-10-12 18:02:42 -0400

    Going to update it

--- studio.hs
+++ studio.hs
@@ -1,26 +1,28 @@
 import Text.Pandoc
 import Control.Monad (forM_, join, mapM, mapM_)
-import System.Directory (getDirectoryContents, createDirectoryIfMissing, setCurrentDirectory, copyFile)
+import Control.Applicative ((<$>))
+import System.Directory (getDirectoryContents, createDirectoryIfMissing, 
+                         setCurrentDirectory, copyFile)
 import System.FilePath (takeExtension, takeFileName, dropExtension)
 import System.IO (writeFile)
 import Data.List (intercalate)
 import Data.Char (toLower)
-import Control.Applicative ((<$>))
 import GHC.Exts (sortWith, groupWith)
 
 data Month 
   = Jan | Feb | Mar | Apr | May | Jun | Jul | Aug | Sep | Oct | Nov | Dec
   deriving (Eq, Ord, Enum, Read, Show)
 
-data Page = Page { article :: FilePath
-                 , static  :: [FilePath]
-                 , date    :: [String] --Month,day,year
+data Page = Page { title :: FilePath    -- Path to words
+                 , static  :: [FilePath]  -- List of static files 
+                 , date    :: [String]    -- Month,day,year
                  } deriving (Show) 
+
 
 main :: IO () 
 main = do
   --Init
-  setCurrentDirectory "/Users/james/Dropbox/Projects/Site/Studio"
+  --setCurrentDirectory "/Users/james/Dropbox/Projects/Site/Studio"
   createDirectoryIfMissing False "Output"
   template <- readFile "template.html"
   
@@ -34,42 +36,29 @@ main = do
   mapM_ (writePage template) pages
 
   --Move over the images
+    --articles
   mapM_ moveStatic pages
-
-  --Table of contents, main page
-  writeTOC template pages
-
-  --Move over static files  
+    --base dir
   files <-  filter ((`elem` [".css",".js"]) . takeExtension) <$> getDirectoryContents "."
   forM_ files (\x -> copyFile x ("Output/" ++ x))
   
-moveStatic :: Page -> IO ()
-moveStatic page = mapM_ func (static page)
-  where func x = copyFile x ("Output/" ++ (last (date page)) ++ "/" ++ (drop 9 x))
-
-writePage :: String -> Page -> IO ()
-writePage template page= do
-  let year = last $ date page  
-  let title =  article page
-
-  mdArt <- readFile ("Articles/" ++ article page ++ "/words.md")
-  let pandocArt = readMarkdown def mdArt
-  let html = writeHtmlString (siteOptions template) pandocArt
-  createDirectoryIfMissing True $ "Output/" ++ year ++ "/" ++ title 
-  writeFile ("Output/" ++ year ++ "/" ++ title ++ "/index.html") html
-
-writeTOC :: String -> [Page] -> IO ()
-writeTOC template pages = do
-  list <- mapM getItem pages 
-  let list' = orderList list
-  let html = writeHtmlString (siteOptions template) (tocWrap list') 
-  writeFile "Output/index.html" html
+  --Table of contents, main page
+  writeTOC template pages
 
 getPages :: [FilePath] -> IO [Page]
 getPages = mapM (\x -> do static <- getStatic ("Articles/" ++ x)
                           date <- getPageDate ("Articles/" ++ x ++ "/words.md")
                           return (Page x static date))
-    
+
+writePage :: String -> Page -> IO ()
+writePage template page= do
+  let year = last $ date page  
+
+  mdArt <- readFile ("Articles/" ++ title page ++ "/words.md")
+  let pandocArt = readMarkdown def mdArt
+  let html = writeHtmlString (siteOptions template) pandocArt
+  createDirectoryIfMissing True $ "Output/" ++ year ++ "/" ++ title page 
+  writeFile ("Output/" ++ year ++ "/" ++ title page ++ "/index.html") html
 
 getPageDate :: FilePath -> IO [String] 
 getPageDate article = do
@@ -82,15 +71,25 @@ getStatic article = do
   list <- getDirectoryContents article
   let list' = filter ((`elem` [".css",".js",".png",".jpg"]) . takeExtension) list
   return $ map ((article ++ "/") ++) list'
-  
+
+moveStatic :: Page -> IO ()
+moveStatic page = mapM_ func (static page)
+  where func x = copyFile x ("Output/" ++ (last (date page)) ++ "/" ++ (drop 9 x))
+ 
+writeTOC :: String -> [Page] -> IO ()
+writeTOC template pages = do
+  list <- mapM getItem pages 
+  let list' = orderList list
+  let html = writeHtmlString (siteOptions template) (tocWrap list') 
+  createDirectoryIfMissing True $ "Output/archives/" 
+  writeFile "Output/archives/index.html" html 
 
 --Write the article and return information for TOC
 getItem :: Page -> IO ([Inline],[Block])
 getItem page = do
-  mdArt <- readFile ("Articles/" ++ article page ++ "/words.md")
+  mdArt <- readFile ("Articles/" ++ title page ++ "/words.md")
   let pandoc = readMarkdown def mdArt
 
-  let title = docTitle $ meta pandoc
   let date = docDate $ meta pandoc  
   let year = unwords $ inlineStr [last date]
 
@@ -98,8 +97,8 @@ getItem page = do
             ([RawInline "html" "<span>"] ++ 
               date ++
                 [RawInline "html" "</span>"] ++ 
-                  [Link title 
-                    ("/" ++ year ++ "/" ++ article page,"")])])
+                  [Link (docTitle $ meta pandoc)
+                    ("/" ++ year ++ "/" ++ title page,"")])])
 
 
 --Converting and then sorting with the first element
@@ -110,26 +109,15 @@ orderList = reverse . snd . unzip . sortWith fst . map ea
   where ea (fs,ls) = (dateOrd $ inlineStr fs, ls)
 
 --Convert docDate into a number to be able to order
---Format: Jan 02 2013
+--Format: Jan 02 2013 = 20130102
 dateOrd :: [String] -> Integer
 dateOrd [month,day,year] = read $ join [year,monthCnvt month,day] 
 
---Convert the month to its repective date number
+--Convert the month to its repective date number in string
 monthCnvt :: String -> String 
-monthCnvt x
-  | "Jan" == x = "01"
-  | "Feb" == x = "02"
-  | "Mar" == x = "03"
-  | "Apr" == x = "04"
-  | "May" == x = "05"
-  | "Jun" == x = "06"
-  | "Jul" == x = "07"
-  | "Aug" == x = "08"
-  | "Sep" == x = "09"
-  | "Oct" == x = "10"
-  | "Nov" == x = "11"
-  | "Dec" == x = "12"
-
+monthCnvt x | length monthNum < 10 = "0" ++ monthNum
+            | otherwise            = monthNum
+  where monthNum = show $ fromEnum (read x :: Month)
 
 --Convert Pandocs Inline to a string with " " for Space, or into words, or
 -- into a urlName format
